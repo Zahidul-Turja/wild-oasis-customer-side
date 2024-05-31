063d6741d20001d68d8b766523932b706d5c1ffa
Zahidul-Turja <zahidulturja@gmail.com>
2024-05-31 22:26:15 +0600

    Metadata and tailwind colors config

--- app/about/page.js
+++ app/about/page.js
@@ -1,3 +1,7 @@
+export const metadata = {
+  title: "About",
+};
+
 export default function Page() {
   return <h1>This is about the wild oasis</h1>;
 }
--- app/layout.js
+++ app/layout.js
@@ -1,14 +1,22 @@
-import Logo from "./components/Logo";
-import Navigation from "./components/Navigation";
+import Logo from "@/app/_components/Logo";
+import Navigation from "@/app/_components/Navigation";
+
+import "@/app/_styles/globals.css";
 
 export const metadata = {
-  title: "The Wild Oasis | Customer Side",
+  // title: "The Wild Oasis | Home",
+  title: {
+    template: "The Wild Oasis | %s",
+    default: "The Wild Oasis | Welcome",
+  },
+  description:
+    "Luxurious cabin hotel, located in the heart of the Italian Dolomites, surronded by beautiful mountains and dark forests",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
+      <body className="bg-primary-950 text-primary-100 min-h-screen">
         <header>
           <Logo />
           <Navigation />
