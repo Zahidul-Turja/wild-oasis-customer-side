f793893cb63a8bee97f6dbcdc3a15982fe8d8010
Zahidul-Turja <zahidulturja@gmail.com>
2024-06-26 20:25:27 +0600

    google session connected with supabase DB

--- app/account/page.js
+++ app/account/page.js
@@ -6,6 +6,9 @@ export const metadata = {
 
 export default async function Page() {
   const session = await auth();
+
+  console.log(session);
+
   const firstName = session?.user?.name.split(" ")[0];
 
   return (
