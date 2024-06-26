98bbc77baaca8f3981bddbfd8a4fc175d4f6d0fa
Zahidul-Turja <zahidulturja@gmail.com>
2024-06-26 20:05:32 +0600

    SignIn signOut

--- app/_components/LoginMessage.js
+++ app/_components/LoginMessage.js
@@ -4,10 +4,10 @@ function LoginMessage() {
   return (
     <div className="grid bg-primary-800 ">
       <p className="text-center text-xl py-12 self-center">
-        Please
+        Please{" "}
         <Link href="/login" className="underline text-accent-500">
           login
-        </Link>
+        </Link>{" "}
         to reserve this
         <br /> cabin right now
       </p>
